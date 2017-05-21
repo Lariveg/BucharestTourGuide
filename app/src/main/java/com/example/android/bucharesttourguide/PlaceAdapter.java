263c50f0c945b12ac5df0e9e59d49159822d34f6
Larisa <l90vegeta@gmail.com>
2017-05-21 15:43:11 +0200

    Done with screenshots

--- app/src/main/java/com/example/android/bucharesttourguide/PlaceAdapter.java
+++ app/src/main/java/com/example/android/bucharesttourguide/PlaceAdapter.java
@@ -26,43 +26,48 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
         mColorResourceId = colorResourceId;
     }
 
+    static class ViewHolder {
+        private TextView nameTextView;
+        private TextView addressTextView;
+        private ImageView placeImageView;
+        private View textContainer;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
-        if(listItemView == null) {
+        ViewHolder holder;
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+            // cache view fields into the holder
+            holder = new ViewHolder();
+            holder.nameTextView = (TextView) listItemView.findViewById(R.id.place_name);
+            holder.addressTextView = (TextView) listItemView.findViewById(R.id.place_address);
+            holder.placeImageView = (ImageView) listItemView.findViewById(R.id.image);
+            holder.textContainer = listItemView.findViewById(R.id.text_container);
+            listItemView.setTag(holder);
+        }
+        // view already exists, get the holder instance from the view
+        else {
+            holder = (ViewHolder) listItemView.getTag();
         }
 
         // Get the {@link Place} object located at this position in the list
         Place currentPlace = getItem(position);
 
-        // Find the TextView in the list_item.xml layout
-        TextView namePlace = (TextView) listItemView.findViewById(R.id.place_name);
-        // Get the version name from the current Word object and
-        // set this text on the name TextView
-        namePlace.setText(currentPlace.getPlaceName());
+        holder.nameTextView.setText(currentPlace.getPlaceName());
+        holder.addressTextView.setText(currentPlace.getPlaceAddress());
+        holder.placeImageView.setImageResource(currentPlace.getImageId());
 
-        // Find the LinearLayout in the list_item.xml layout with the ID version_number
-        TextView addressPlace = (TextView) listItemView.findViewById(R.id.place_address);
-        // Get the version number from the current Place object and
-        // set this text on the number TextView
-        addressPlace.setText(currentPlace.getPlaceAddress());
-
-        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
-        imageView.setImageResource(currentPlace.getImageId());
-
-
-        // Set the theme color for the list item
-        View textContainer = listItemView.findViewById(R.id.text_container);
         // Find the color that the resource ID maps to
         int color = ContextCompat.getColor(getContext(), mColorResourceId);
         // Set the background color of the text container View
-        textContainer.setBackgroundColor(color);
+        holder.textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
